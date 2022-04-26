205d80b8ae5b7ad13ccbbe009a0055f1a02d8fdc
Jesús Renato Rivas Andrade <99424717+Renato-Rivas@users.noreply.github.com>
2022-04-26 10:23:47 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final appTitle = 'Drawer Demo';
+  final appTitle = 'Practica Drawer';
 
   @override
   Widget build(BuildContext context) {
